15ab9a9e119cacf3a91525189adbd2877d5af4b0
yskmasumoto <msmtprogram0615@gmail.com>
2025-12-24 22:50:18 +0900

    入力でcanvasの縦横幅を受け取る

--- cmd/asciigis/main.go
+++ cmd/asciigis/main.go
@@ -10,17 +10,24 @@ import (
 
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [geojson path]\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [options] [geojson path]\n", os.Args[0])
 		flag.PrintDefaults()
 		fmt.Fprintf(flag.CommandLine.Output(), "\nExamples:\n  %s /path/to/data.geojson\n  %s   # start then enter path interactively\n", os.Args[0], os.Args[0])
 	}
+
+	var mapWidth int
+	var mapHeight int
+	flag.IntVar(&mapWidth, "W", 0, "Fixed canvas width (cells). 0 = auto")
+	flag.IntVar(&mapWidth, "width", 0, "Fixed canvas width (cells). 0 = auto")
+	flag.IntVar(&mapHeight, "H", 0, "Fixed canvas height (cells). 0 = auto")
+	flag.IntVar(&mapHeight, "height", 0, "Fixed canvas height (cells). 0 = auto")
 
 	flag.Parse()
 	geoPath := ""
 	if flag.NArg() >= 1 {
 		geoPath = flag.Arg(0)
 	}
-	if err := tui.Run(geoPath); err != nil {
+	if err := tui.RunWithOptions(geoPath, tui.Options{MapWidth: mapWidth, MapHeight: mapHeight}); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
